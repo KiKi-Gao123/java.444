0c69d88e7119e05842f54133f920010f93607225
Gao-code <1036546275@qq.com>
2020-12-10 18:36:54 +0800

    initial commit

---
+++ src/gao.java
@@ -0,0 +1,27 @@
+public class gao { {
+    public static void main(String[ ] argus){
+    {char a ='\u6700';
+    char b ='\u8fd1';
+    char c ='\u5f88';
+    char d ='\u5fd9';
+    char e ='\u5427';
+    char f='，';
+    char g='\u52a0';
+    char h='\u6cb9';
+    char i='\u5440';
+    char j='！';
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+        System.out.print(e);
+        System.out.print(f);
+        System.out.print(g);
+        System.out.print(h);
+        System.out.print(i);
+        System.out.print(j);
+    }
+
+}
+}
